aca4f256fc0543c22df4a0121f0fc6229faaa6ac
lasconcore <laurence.conwy@gmail.com>
2025-05-03 13:26:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFzY29uY29yZSIsImEiOiJjbWE1cHZncHgwanZiMmpxdXFzdjY3aTN2In0.KHIXaS5McVLHm4PqB2nkkA';
+
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the map container
+  style: 'mapbox://styles/lasconcore/cma5bbtvu003r01sd7yy51bro',
+  center: [-4.2818, 55.8687], // Center at Glasgow
+  zoom: 13 // Initial zoom level
+});
+
+// Add geolocate control to the map.
+map.addControl(
+  new mapboxgl.GeolocateControl({
+      positionOptions: {
+          enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+  })
+);
+
+// Hamburger menu toggle functionality
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  menuToggle.classList.toggle('open');
+});
+
+const aboutLink = document.querySelector('a[href="#about"]');
+const aboutText = document.querySelector('.about-text');
+
+aboutLink.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevent default anchor behavior
+  aboutText.classList.toggle('visible');
+});
+
+const contactLink = document.querySelector('a[href="#contact"]');
+const contactText = document.querySelector('.contact-text');
+
+contactLink.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevent default anchor behavior
+  contactText.classList.toggle('visible');
+});
+
+const mapLink = document.querySelector('a[href="#map"]');
+const mapText = document.querySelector('.map-text');
+
+mapLink.addEventListener('click', (e) => {
+  e.preventDefault(); // Prevent default anchor behavior
+  mapText.classList.toggle('visible');
+});
